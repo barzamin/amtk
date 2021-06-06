06c97ff628071689a6e9d3a18eccde696baedf0a
Erin Moon <erin@hecke.rs>
2021-06-05 22:19:38 -0400

    decryption tool

---
+++ src/bin/decrypt.rs
@@ -0,0 +1,9 @@
+use std::{env, fs};
+use amtk::decrypt;
+
+fn main() {
+    let p = env::args().nth(1).expect("pass a path");
+    let d = fs::read_to_string(p).expect("can't read");
+    
+    println!("{}", decrypt(&d));
+}
